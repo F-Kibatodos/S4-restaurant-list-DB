652ce30fcd943be7823f27b618b98d5a4667c13c
Brian.L <brian9500885@gmail.com>
2019-06-22 11:31:41 +0800

    Add searching function

--- app.js
+++ app.js
@@ -46,6 +46,23 @@ app.get('/', (req, res) => {
     })
 })
 
+// 搜尋功能
+app.get('/search', (req, res) => {
+  let keyword = req.query.keyword
+  //$or 是「或」條件; $regex 導入變數; options為 "i"設定找到結果是 "insensitive"
+  Restaurant.find({
+    $or: [
+      { name: { $regex: keyword, $options: 'i' } },
+      { category: { $regex: keyword, $options: 'i' } }
+    ]
+  })
+    .sort({ _id: 1 })
+    .exec((err, restaurant) => {
+      if (err) console.error(err)
+      res.render('index', { restaurant, keyword })
+    })
+})
+
 // 新增一間餐廳頁面
 app.get('/restaurants/new', (req, res) => {
   res.render('new')
