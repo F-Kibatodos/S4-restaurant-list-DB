e65defb4b6a088cbdeb95353fa2605dbbcd4787a
Brian.L <brian9500885@gmail.com>
2019-06-29 13:33:15 +0800

    update seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -6,7 +6,10 @@ mongoose.connect('mongodb://localhost/restaurants', {
 // 將資料庫回傳的文件儲存至變數
 const db = mongoose.connection
 const Restaurant = require('../restaurant')
+const User = require('../user')
 const restaurantList = require('../../restaurant.json').results
+const users = require('../../user.json').results
+const bcrypt = require('bcryptjs')
 
 // 與資料庫連線失敗訊息
 db.on('error', () => {
@@ -15,7 +18,74 @@ db.on('error', () => {
 // 與資料庫連線成功訊息
 db.once('open', () => {
   console.log('db connected')
-  for (let item of restaurantList) {
+  const newUser = new User({
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678'
+  })
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err
+      newUser.password = hash
+      newUser
+        .save()
+        .then(user => {
+          for (let i = 0; i < 3; i++) {
+            Restaurant.create({
+              name: restaurantList[i].name,
+              name_en: restaurantList[i].name_en,
+              category: restaurantList[i].category,
+              image: restaurantList[i].image,
+              location: restaurantList[i].location,
+              phone: restaurantList[i].phone,
+              google_map: restaurantList[i].google_map,
+              rating: restaurantList[i].rating,
+              description: restaurantList[i].description,
+              userId: user._id
+            })
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    })
+  })
+  const newUser2 = new User({
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678'
+  })
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err
+      newUser2.password = hash
+      newUser2
+        .save()
+        .then(user => {
+          for (let i = 3; i < 6; i++) {
+            Restaurant.create({
+              name: restaurantList[i].name,
+              name_en: restaurantList[i].name_en,
+              category: restaurantList[i].category,
+              image: restaurantList[i].image,
+              location: restaurantList[i].location,
+              phone: restaurantList[i].phone,
+              google_map: restaurantList[i].google_map,
+              rating: restaurantList[i].rating,
+              description: restaurantList[i].description,
+              userId: user._id
+            })
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    })
+  })
+  console.log('done')
+})
+/*
+for (let item of restaurantList) {
     Restaurant.create({
       name: item.name,
       name_en: item.name_en,
@@ -28,4 +98,4 @@ db.once('open', () => {
       description: item.description
     })
   }
-})
+*/
